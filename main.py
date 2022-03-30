9c2af3143688da70b28ba9990a1f8703b2ecaec1
Alex-D-G <60549983+Alex-D-G@users.noreply.github.com>
2022-03-30 15:40:41 +0200

    Changed the comments

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import compiler
 f = open("codeExample.txt", "r")
 codeList = f.readlines()
 
-# This external code from the interpreter runs the required functions
+# Run the Compiler
 value, varList, funcList, checkpointList, resultStr, additionalStr = compiler.run_code(codeList, 0, [], [], [], 0, "", False, False)
 
